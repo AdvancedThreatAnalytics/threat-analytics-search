e03682e4621d38d9d11327226967e96abdecd129
Jonathan Seawright <jonathan.seawright@criticalstart.com>
2021-05-06 14:13:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/AdvancedThreatAnalytics/threat-analytics-search/blob/main/LICENSE)
+# Threat Analytics Search Chrome extension
+
+The **Threat Analytics Search** extension allows Google Chrome users to highlight specific text and conduct searches using various services. For instance, if you have a domain name you want to research, you could highlight that domain name and automatically search across as many registrars or threat exchanges you want.
+
+## Features
+
+### Configuration files
+
+Once the extension is installed, it will attempt to download a configuration file with the latest recommended settings. If the file is not available or can't be downloaded, it will use default settings.
+
+By default, the extension uses a configuration file hosted in this repository, but it can be configured to fetch it from any URL.
+
+If needed, configuration files can be encrypted using AES-CSC. You would then provide the decryption key in the extension settings.
+
+Optionally, there is a setting to automatically re-fetch the configuration file once a week.
+
+### Search providers
+
+For each search provider, the extension will generate an item in the context menu, so that provider can be used to search the selected texts.
+
+> There are two types of search providers:
+>
+> - GET providers, that will perform the search by opening the provider's site in a new tab (where the result will be shown).
+> - POST providers, that will perform the search by executing a POST request (on the provider's server) and show the result in a dialog.
+
+The extension also adds "groups" to the context menu. Clicking on these items will conduct the search using several search providers at the same time.
+
+If configured, the extension also defines three special sections on the contextual menu to conduct searches on [Carbon Black](https://www.carbonblack.com/), [NetWitness Investigator](http://www.emc.com/security/security-analytics/security-analytics.htm) and [RSA Security Analytics](https://community.rsa.com/t5/rsa-netwitness-investigator/tkb-p/netwitness-investigator).
+
+## Development
+
+The extension is implemented with JavaScript, using the following libraries
+
+| Library                                  | Used for                      |
+|------------------------------------------|-------------------------------|
+| [Mustache](https://mustache.github.io/)  | UI                            |
+| AES-CBC                                  | Configuration file encryption |
+| [Luxon](https://moment.github.io/luxon/) | Utility                       |
+| [Lodash](https://lodash.com/)            | Utility                       |
+| [Bootstrap](https://getbootstrap.com/)   | UI                            |
+
+### Code structure
+
+The application is divided into three sections:
+
+- The background page
+    - Initializing the extension
+    - Downloading and parsing the configuration file
+    - Updating the context menu
+- The post-handler page
+    - Executing searches that require doing a POST request
+- The options page
+    - Allows users to modify extension settings
+    - Adding or removing search providers.
+
+Additionally, there is a migration page that is used to relocate the user's settings from the local storage, i.e. `localStorage`, to Chrome's storage, i.e. `chrome.storage.local`. This change was required after upgrading the manifest file to version 3 since service workers (unlike background pages) do not have access to the local storage.
+
+### Packaging
+
+To package and distribute the extension see the [Chrome documentation](https://developer.chrome.com/docs/extensions/mv3/hosting/).
---
+++ js/migration.js
@@ -0,0 +1,115 @@
+/**
+ * This script is run when updating the extension from version 4.x.x to version 5.0.0 (or higher) and
+ * his main tasks is to relocate the user's settings saved from the local storage (i.e. "localStorage")
+ * to the Chrome's storage (i.e. "chrome.storage.local").
+ *
+ * The need for this change is because starting from version 5.0.0 the extension uses Manifest v3
+ * which don't longer supports background pages, and instead uses services workers (and since service
+ * workers don't has access to the local storage, we must move all data saved there into Chrome's 
+ * storage which can be accessed by both service workers and standard scripts).
+ */
+
+// These URLs are deprecated, default settings should be read from the GitHub repository.
+var OBSOLETES_CONFIG_URL = [
+  "http://www.criticalstart.com/cschromeplugin/criticalstart.txt",
+  "https://www.criticalstart.com/wp-content/uploads/2018/02/criticalstart.txt"
+];
+
+// Wait for the page to be loaded to execute the initialization function.
+document.addEventListener("DOMContentLoaded", async function() {
+  // Get default settings.
+  window.defaultFile = await ConfigFile.getDefaultJSON();
+
+  // Migrate data.
+  await Promise.all([
+    migrateGeneralSettings(),
+    migrateSearchProviders(),
+    migrateSpecialProvider(StoreKey.CARBON_BLACK, "CBC", "_CBCConfig", "_CBCallquery"),
+    migrateSpecialProvider(StoreKey.NET_WITNESS, "NWI", "_NWIConfig", "_NWIallquery"),
+    migrateSpecialProvider(StoreKey.RSA_SECURITY, "RSA", "_RSAConfig", "_RSAallquery"),
+    migrateOthers(),
+  ]);
+
+  // Update contextual menu.
+  chrome.runtime.sendMessage({ action: "updateContextualMenu" });
+
+  // Redirect to welcome URL.
+  window.open(MiscURLs.INSTALLED_URL, "_self");
+});
+
+function migrateGeneralSettings() {
+  var defaultBasic = ConfigFile.parseBasicSettings(defaultFile.config);
+  var defaultGroups = ConfigFile.parseGroups(defaultFile.groups);
+
+  // If previous URL is empty or has a deprecated value change it to default.
+  var configUrl = Storage.getItem("_configUrl");
+  if(_.isEmpty(configUrl) || OBSOLETES_CONFIG_URL.indexOf(configUrl) >= 0) {
+    configUrl = defaultBasic.configurationURL;
+  }
+
+  return LocalStore.setOne(StoreKey.SETTINGS, {
+    configurationURL: configUrl,
+    useGroups: Storage.getItem("_configUseGroups") === "true" || _.get(defaultBasic, 'useGroups', false),
+    configEncrypted: Storage.getItem("_configEnc") === "true",
+    configEncryptionKey: Storage.getItem("_configEncKey") || null,
+    autoUpdateConfig: Storage.getItem("_configAutoRefresh") === "true" || _.get(defaultBasic, 'autoUpdateConfig', false),
+
+    providersGroups: [
+      {
+        name: Storage.getItem("_group1Name") || _.get(defaultGroups, '0.name'),
+        enabled: true
+      },
+      {
+        name: Storage.getItem("_group2Name") || _.get(defaultGroups, '1.name'),
+        enabled: true
+      },
+      {
+        name: Storage.getItem("_group3Name") || _.get(defaultGroups, '2.name'),
+        enabled: Storage.getItem("_enableGroup3") == "true"
+      },
+    ],
+
+    // Note that these options aren't defined by configuration files.
+    resultsInBackgroundTab: Storage.getItem("_askbg") !== "false",
+    enableAdjacentTabs: Storage.getItem("_asknext") !== "false",
+    openGroupsInNewWindow: Storage.getItem("_asknewwindow") !== "false",
+    enableOptionsMenuItem: Storage.getItem("_askoptions") !== "false"
+  });
+}
+
+function migrateSearchProviders() {
+  return LocalStore.setOne(
+    StoreKey.SEARCH_PROVIDERS,
+    _.map(
+      tryJSONparse(Storage.getItem("_allsearch")) || _.get(defaultFile, 'searchproviders', []),
+      ConfigFile.parseProvider
+    )
+  );
+}
+
+function migrateSpecialProvider(storeKey, fileKey, configKey, queryKey) {
+  return LocalStore.setOne(storeKey, {
+    config: tryJSONparse(Storage.getItem(configKey)) || _.get(defaultFile, `${fileKey}.Config`, {}),
+    queries: _.map(
+      tryJSONparse(Storage.getItem(queryKey)) || _.get(defaultFile, `${fileKey}.Queries`, []),
+      ConfigFile.parseQuery
+    )
+  });
+}
+
+function migrateOthers() {
+  return LocalStore.setOne(
+    StoreKey.LAST_CONFIG_UPDATE,
+    Storage.getItem("_configLastRefresh")
+  );
+}
+
+function tryJSONparse(string) {
+  var res;
+  try {
+    res = JSON.parse(string);
+  } catch(err) {
+    res = null;
+  }
+  return res;
+}

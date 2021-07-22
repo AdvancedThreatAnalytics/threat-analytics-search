71288fecd6512ac4d646cb6097367aebfef6f839
tim-kodirov <timi4.uz@mail.ru>
2021-07-22 14:45:33 +0500

    Issue #69 - Fix context menu sample data

--- tests/unit/contextMenu.spec.js
+++ tests/unit/contextMenu.spec.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 
 require("./util");
 
-const ConfigFile = require("../../src/js/shared/config_file");
+const ConfigFile = require("../../src/js/shared/config_file").default;
 const LocalStore = require("../../src/js/shared/local_store").default;
 const { StoreKey } = require("../../src/js/shared/constants");
 const { ContextualMenu, MenuPreffix } = require("../../src/background");
@@ -12,7 +12,7 @@ const createContextMenu = jest.spyOn(chrome.contextMenus, "create");
 
 describe("ContextualMenu", () => {
   beforeAll(async () => {
-    await ConfigFile.default.sanitizeSettings();
+    await ConfigFile.sanitizeSettings();
   });
 
   it("Groups should be added if 'useGroups' is enabled", async () => {
@@ -42,17 +42,48 @@ describe("ContextualMenu", () => {
   });
 
   it("One menu item should be added for each non-disabled search provider", async () => {
-    // Get search providers
-    const searchProviders =
-      (await LocalStore.getOne(StoreKey.SEARCH_PROVIDERS)) || [];
+    const sampleData = [
+      {
+        menuIndex: "searchprovider-0",
+        label: "Test 1",
+        link: "http://www.test.com/?q=TESTSEARCH",
+        enabled: true,
+        fromConfig: true,
+        group: 3,
+        postEnabled: false,
+        postValue: "",
+        proxyEnabled: false,
+        proxyUrl: "",
+      },
+      {
+        menuIndex: "searchprovider-1",
+        label: "Test 2",
+        link: "http://www.test.com/?q=TESTSEARCH",
+        enabled: false,
+        fromConfig: true,
+        group: 3,
+        postEnabled: false,
+        postValue: "",
+        proxyEnabled: false,
+        proxyUrl: "",
+      },
+    ];
+    // Set search providers
+    await LocalStore.setOne(StoreKey.SEARCH_PROVIDERS, sampleData);
 
     // Call update
     await ContextualMenu.update();
 
-    // Check if non-disabled providers are added
-    searchProviders.forEach((provider, index) => {
+    // Check if only non-disabled providers are added
+    sampleData.forEach((provider, index) => {
       if (provider.enabled) {
         expect(createContextMenu).toBeCalledWith({
+          id: MenuPreffix.PROVIDER + index,
+          title: provider.label,
+          contexts: ["selection"],
+        });
+      } else {
+        expect(createContextMenu).not.toBeCalledWith({
           id: MenuPreffix.PROVIDER + index,
           title: provider.label,
           contexts: ["selection"],
@@ -62,25 +93,105 @@ describe("ContextualMenu", () => {
   });
 
   it("One menu item should be added for each non-disabled special provider", async () => {
+    const sampleData = {
+      [StoreKey.CARBON_BLACK]: {
+        config: {
+          CBCConfigEnable: false,
+          CBCConfigPopup: false,
+          CBCConfigUseHttps: true,
+          CBCConfigNewTab: true,
+          CBCConfigHost: "192.168.1.10",
+          CBCConfigPort: "",
+          CBCConfigURLVersion: "1",
+        },
+        queries: [
+          {
+            menuIndex: -1,
+            label: "Search All (Mostly Use This)",
+            query: "q=TESTSEARCH",
+            enabled: true,
+          },
+          {
+            menuIndex: -1,
+            label: "Domain Name (FQDN)",
+            query: "cb.q.domain=TESTSEARCH",
+            enabled: false,
+          },
+        ],
+      },
+      [StoreKey.NET_WITNESS]: {
+        config: {
+          NWIConfigEnable: true,
+          NWIConfigPopup: false,
+          NWIConfigGMT: false,
+          NWIConfigHost: "",
+          NWIConfigPort: "",
+          NWIConfigCollectionName: "",
+          NWIConfigRange1: "1",
+          NWIConfigRange2: "24",
+          NWIConfigRange3: "48",
+          NWIConfigRange4: "720",
+        },
+        queries: [],
+      },
+      [StoreKey.RSA_SECURITY]: {
+        config: {
+          RSAConfigEnable: true,
+          RSAConfigPopup: false,
+          RSAConfigUseHttps: true,
+          RSAConfigNewTab: true,
+          RSAConfigHost: "192.168.1.10",
+          RSAConfigPort: "",
+          RSAConfigDevId: "2",
+          RSAConfigRange1: "1",
+          RSAConfigRange2: "24",
+          RSAConfigRange3: "48",
+          RSAConfigRange4: "720",
+        },
+        queries: [
+          {
+            menuIndex: -1,
+            label: "Search Hostname",
+            query: "alias.host='TESTSEARCH'",
+            enabled: true,
+          },
+          {
+            menuIndex: -1,
+            label: "Search Source IP",
+            query: "ip.src=TESTSEARCH",
+            enabled: false,
+          },
+        ],
+      },
+    };
+
     const providers = [
       {
         storeKey: StoreKey.CARBON_BLACK,
         enableKey: "CBCConfigEnable",
+        title: "Carbon Black",
       },
       {
         storeKey: StoreKey.NET_WITNESS,
         enableKey: "NWIConfigEnable",
+        title: "NetWitness Investigator",
       },
       {
         storeKey: StoreKey.RSA_SECURITY,
         enableKey: "RSAConfigEnable",
+        title: "RSA Security Analytics",
       },
     ];
+
+    // Set special providers
+    for (const storeKey in sampleData) {
+      await LocalStore.setOne(storeKey, sampleData[storeKey]);
+    }
 
     // Call update
     await ContextualMenu.update();
 
-    // Check if the provider's menu items are added (if enabled).
+    // Check if only non-disabled providers are added
     for (const provider of providers) {
       const settings = await LocalStore.getOne(provider.storeKey);
       const config = _.get(settings, "config", {});
@@ -89,22 +200,36 @@ describe("ContextualMenu", () => {
       if (config[provider.enableKey]) {
         expect(createContextMenu).toBeCalledWith(
           expect.objectContaining({
-            title: "Carbon Black",
+            title: provider.title,
             contexts: ["selection"],
           })
         );
+      } else {
+        expect(createContextMenu).not.toBeCalledWith(
+          expect.objectContaining({
+            title: provider.title,
+            contexts: ["selection"],
+          })
+        );
+      }
 
-        // Check if non-disabled query items are added
-        for (const index in queries) {
-          const query = queries[index];
-          if (query.enabled !== false && query.enabled !== "false") {
-            expect(createContextMenu).toBeCalledWith(
-              expect.objectContaining({
-                title: query.label,
-                contexts: ["selection"],
-              })
-            );
-          }
+      // Check if only non-disabled query items from non-disabled providers are added
+      for (const index in queries) {
+        const query = queries[index];
+        if (config[provider.enableKey] && query.enabled) {
+          expect(createContextMenu).toBeCalledWith(
+            expect.objectContaining({
+              title: query.label,
+              contexts: ["selection"],
+            })
+          );
+        } else {
+          expect(createContextMenu).not.toBeCalledWith(
+            expect.objectContaining({
+              title: query.label,
+              contexts: ["selection"],
+            })
+          );
         }
       }
     }

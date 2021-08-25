694e7f39315142fa38ee8200fba1d4d3a7565c49
tim-kodirov <timi4.uz@mail.ru>
2021-08-26 00:22:08 +0500

    Issue #65 - Fix checking providers

--- tests/e2e/migration.spec.js
+++ tests/e2e/migration.spec.js
@@ -75,6 +75,12 @@ describe("Migration (from v4.0)", () => {
       () => new Promise((resolve) => chrome.storage.local.get(null, resolve))
     );
 
+    // Update menu index to its initial value as
+    // it could have been edited in background.js
+    _.forEach(chromeData[StoreKey.SEARCH_PROVIDERS], (item, index) => {
+      item.menuIndex = _.get(sampleData, `_allsearch.${index}.0`);
+    });
+
     const expectedData = {
       [StoreKey.SETTINGS]: {
         configurationURL: sampleData._configUrl,
@@ -109,7 +115,7 @@ describe("Migration (from v4.0)", () => {
 
       [StoreKey.SEARCH_PROVIDERS]: [
         {
-          menuIndex: "searchprovider-0",
+          menuIndex: sampleData._allsearch[0][0],
           label: sampleData._allsearch[0][1],
           link: sampleData._allsearch[0][2],
           enabled: sampleData._allsearch[0][3],
@@ -164,6 +170,12 @@ describe("Migration (from v4.0)", () => {
       () => new Promise((resolve) => chrome.storage.local.get(null, resolve))
     );
 
+    // Update menu index to its initial value as
+    // it could have been edited in background.js
+    _.forEach(chromeData[StoreKey.SEARCH_PROVIDERS], (item, index) => {
+      item.menuIndex = _.get(SETTINGS, `searchproviders.${index}.0`);
+    });
+
     const defaultBasic = ConfigFile.parseBasicSettings(SETTINGS.config);
     const defaultGroups = ConfigFile.parseGroups(SETTINGS.groups);
 
@@ -200,10 +212,7 @@ describe("Migration (from v4.0)", () => {
       },
 
       [StoreKey.SEARCH_PROVIDERS]: ConfigFile.parseProviders(
-        _.get(SETTINGS, "searchproviders", []).map((item, index) => {
-          item[0] = item[3] ? `searchprovider-${index}` : -1;
-          return item;
-        })
+        _.get(SETTINGS, "searchproviders", [])
       ),
 
       [StoreKey.CARBON_BLACK]: {

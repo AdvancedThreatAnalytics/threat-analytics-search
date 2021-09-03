d31ddc6f2a83e7442359e0f468ebf856685f794e
Saidmurod1996 <saidkhonovsaidmurod@gmail.com>
2021-09-03 23:28:09 +0500

    Issue #87 - implement encrypt function

---
+++ tests/unit/encryption.spec.js
@@ -0,0 +1,11 @@
+require("./util");
+const { decryptAES, encryptAES } = require("../../src/js/shared/encryption");
+const defaultSettings = require("../resources/defaultSettings.json");
+
+describe("Encryption", () => {
+  it("Test encryptAES and decryptAES functions", async () => {
+    const encryptedText = encryptAES(JSON.stringify(defaultSettings), "password");
+    const decryptedText = decryptAES(encryptedText, "password");
+    expect(JSON.parse(decryptedText)).toStrictEqual(defaultSettings);
+  });
+});

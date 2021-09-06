2d867842b79c05085bcd1b2a22fb3fc2337c9f99
Saidmurod1996 <saidkhonovsaidmurod@gmail.com>
2021-09-07 02:46:39 +0500

    Issue #87 - lint

--- tests/unit/encryption.spec.js
+++ tests/unit/encryption.spec.js
@@ -4,7 +4,10 @@ const defaultSettings = require("../resources/defaultSettings.json");
 
 describe("Encryption", () => {
   it("Test encryptAES and decryptAES functions", async () => {
-    const encryptedText = encryptAES(JSON.stringify(defaultSettings), "password");
+    const encryptedText = encryptAES(
+      JSON.stringify(defaultSettings),
+      "password"
+    );
     const decryptedText = decryptAES(encryptedText, "password");
     expect(JSON.parse(decryptedText)).toStrictEqual(defaultSettings);
   });

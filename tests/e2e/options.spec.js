d91c158f728fe892154c258e06070d01ea5fbd4b
tim-kodirov <timi4.uz@mail.ru>
2021-09-20 15:05:21 +0500

    Issue #92 - e2e test for search provider order

--- tests/e2e/options.spec.js
+++ tests/e2e/options.spec.js
@@ -37,6 +37,35 @@ describe("Options page", () => {
     expect(value).toEqual(newValue);
   });
 
+  test("Order changes on providers are preserved", async () => {
+    // Wait until the input appears.
+    let selector =
+      "form[name=manage_providers] ul[role=list] li[role=listitem] input[name=label_1]";
+    await page.waitForSelector(selector);
+
+    // Get the input value
+    const expectedValue = await page.$eval(selector, (el) => el.value);
+
+    // Drag and drop second item to the first
+    const dragSelector =
+      "form[name=manage_providers] ul[role=list] li[role=listitem] .sortable-handle";
+    const elements = await page.$$(dragSelector);
+    elements[1].dragAndDrop(elements[0]);
+
+    // Reload the page.
+    await page.reload();
+
+    // Wait until the input appears.
+    selector =
+      "form[name=manage_providers] ul[role=list] li[role=listitem] input[name=label_0]";
+    await page.waitForSelector(selector);
+
+    // Get the input value
+    const value = await page.$eval(selector, (el) => el.value);
+
+    expect(value).toEqual(expectedValue);
+  });
+
   afterAll(async () => {
     await browser.close();
   });

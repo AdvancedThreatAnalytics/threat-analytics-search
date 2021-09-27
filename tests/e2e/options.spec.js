41e54ec224e9047315cbcfee55d71076555ac20d
Temur Kodirov <timi4.uz@mail.ru>
2021-09-27 19:06:53 +0500

    Issue #92 - Add E2E test for change order of Search Providers (#106)
    * Issue #92 - e2e test for search provider order
    
    * Issue #92 and PIX-310 - Fix e2e test

--- tests/e2e/options.spec.js
+++ tests/e2e/options.spec.js
@@ -37,6 +37,38 @@ describe("Options page", () => {
     expect(value).toEqual(newValue);
   });
 
+  test("Changes on providers' order are preserved", async () => {
+    const listItemSelector = "form[name=manage_providers] ul[role=list] li[role=listitem]";
+
+    // Wait until the input appears.
+    let selector = `${listItemSelector} input[name=label_1]`;
+    await page.waitForSelector(selector);
+
+    // Enable drag interception (otherwise, drag and drop feature doesn't work)
+    await page.setDragInterception(true); 
+
+    // Get the input value
+    const expectedValue = await page.$eval(selector, (el) => el.value);
+
+    // Drag and drop second item to the first (change order)
+    const dragSelector = `${listItemSelector} .sortable-handle`;
+    const elements = await page.$$(dragSelector);
+    await elements[1].dragAndDrop(elements[0]);
+
+    // Reload the page.
+    await page.reload();
+
+    // Wait until the input appears.
+    selector = `${listItemSelector} input[name=label_0]`;
+    await page.waitForSelector(selector);
+
+    // Get the input value
+    const value = await page.$eval(selector, (el) => el.value);
+
+    // Check if order change is preserved
+    expect(value).toEqual(expectedValue);
+  });
+
   test("Link from Security Analytics Investigation autofills settings correctly", async () => {
     const newValue = "https://security.com:81/investigation/12/";
 

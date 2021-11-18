9fd56aa1b8a164f2884ee8c9d01ea3188e9da8c0
Pushpendra <pushpendra.khandelwal@criticalstart.com>
2021-11-18 22:03:06 +0530

    Issue #123 - Remove Mixpanel related code (#124)
    * Issue #113 | Remove mixpanel related  code
    
    add BROWSER_NAME and UPDATE_URL in env files
    
    * use Dotenv to read .env files

--- webpack.config.js
+++ webpack.config.js
@@ -2,109 +2,108 @@ const path = require("path");
 const bomPlugin = require("webpack-utf8-bom");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
-const Dotenv = require("dotenv-webpack");
+const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
+const Webpack = require("webpack");
+const Dotenv = require("dotenv");
 
-module.exports = (env) => ({
-  entry: {
-    background: "./src/background.js",
-    "js/migration": "./src/js/migration.js",
-    "js/options": "./src/js/options.js",
-    "js/postHandler": "./src/js/postHandler.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    publicPath: "./",
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(svg|eot|woff|woff2|ttf)$/,
-        use: ["file-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/options.html",
-      filename: "options.html",
-      chunks: ["js/options"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/postHandler.html",
-      filename: "postHandler.html",
-      chunks: ["js/postHandler"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/migration.html",
-      filename: "migration.html",
-      chunks: ["js/migration"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/views/providers.html",
-      filename: "views/providers.html",
-    }),
-    new HtmlReplaceWebpackPlugin([
-      {
-        pattern: "@@browserName",
-        replacement: env.mode === "edge" ? "Edge" : "Chrome"
-      },
-    ]),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = (env) => (
+  Dotenv.config({ path: `./${env.mode}.env` }),
+  {
+    entry: {
+      background: "./src/background.js",
+      "js/migration": "./src/js/migration.js",
+      "js/options": "./src/js/options.js",
+      "js/postHandler": "./src/js/postHandler.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+      publicPath: "./",
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
         {
-          // Copy assets from 'src/' and settings file to 'dist/' folder.
-          from: path.join(__dirname, "src/"),
-          globOptions: {
-            ignore: [
-              "**/*.+(css|js)",
-              "**/migration.html",
-              "**/options.html",
-              "**/postHandler.html",
-              "**/providers.html"
-            ],
-          },
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
         {
-          from: path.join(__dirname, "src/manifest.json"),
-          transform(content) {
-            return content
-              .toString()
-              .replace(
-                "process.env.update_url",
-                env.mode === "edge"
-                  ? "https://edge.microsoft.com/extensionwebstorebase/v1/crx"
-                  : "https://clients2.google.com/service/update2/crx"
-              )
-          }
+          test: /\.scss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
         {
-          from: "./settings.json",
+          test: /\.(svg|eot|woff|woff2|ttf)$/,
+          use: ["file-loader"],
         },
       ],
-    }),
-    new bomPlugin(true),
-    new Dotenv({
-      path: `./${env.mode}.env`,
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      chunks(chunk) {
-        // Dont split background file, otherwise won't be loaded completely by Chrome.
-        return chunk.name !== "background";
+    },
+    plugins: [
+      new Webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env),
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/options.html",
+        filename: "options.html",
+        chunks: ["js/options"],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/postHandler.html",
+        filename: "postHandler.html",
+        chunks: ["js/postHandler"],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/migration.html",
+        filename: "migration.html",
+        chunks: ["js/migration"],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/views/providers.html",
+        filename: "views/providers.html",
+      }),
+      new HtmlReplaceWebpackPlugin([
+        {
+          pattern: "@@browserName",
+          replacement: process.env.BROWSER_NAME,
+        },
+      ]),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            // Copy assets from 'src/' and settings file to 'dist/' folder.
+            from: path.join(__dirname, "src/"),
+            globOptions: {
+              ignore: [
+                "**/*.+(css|js)",
+                "**/migration.html",
+                "**/options.html",
+                "**/postHandler.html",
+                "**/providers.html",
+              ],
+            },
+          },
+          {
+            from: path.join(__dirname, "src/manifest.json"),
+            transform(content) {
+              return content
+                .toString()
+                .replace("process.env.update_url", process.env.UPDATE_URL);
+            },
+          },
+          {
+            from: "./settings.json",
+          },
+        ],
+      }),
+      new bomPlugin(true),
+    ],
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        chunks(chunk) {
+          // Dont split background file, otherwise won't be loaded completely by Chrome.
+          return chunk.name !== "background";
+        },
       },
     },
-  },
-});
+  }
+);

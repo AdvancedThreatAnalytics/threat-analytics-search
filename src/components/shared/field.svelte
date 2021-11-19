60f19ef71a396d74cec9ec448f03deca8fe784ef
tim-kodirov <temur.kodirov@criticalstart.com>
2021-11-19 15:19:47 +0500

    Issue PIX-468 #115 - Minor fixes

--- src/components/shared/field.svelte
+++ src/components/shared/field.svelte
@@ -2,15 +2,25 @@
 import BSN from "bootstrap.native/dist/bootstrap-native.esm.min.js";
 import { createEventDispatcher, onMount } from "svelte";
 
+const dispatch = createEventDispatcher();
+
+// Props
 export let item = {};
 export let value = null;
 
-const dispatch = createEventDispatcher();
+// Auxiliary variables
+let dropdown = null;
 
+// Bindings
+let dropdownButton;
+let popoverIcon;
+
+// Computed variables
 const isCheckbox = item.type === "checkbox";
 const isInput = item.type === "input";
 const isDropdown = item.type === "dropdown";
 
+// Methods
 function onChange(newValue) {
   if (newValue !== value) {
     dispatch("change", newValue);
@@ -19,18 +29,16 @@ function onChange(newValue) {
   dropdown.toggle();
 }
 
-let dropdown = null;
+// Hooks
 onMount(() => {
   if (isDropdown) {
     // Initialize dropdown
-    dropdown = new BSN.Dropdown(
-      document.getElementById(`settings_${item.key}`)
-    );
+    dropdown = new BSN.Dropdown(dropdownButton);
 
     // Initialize popover
     const providers =
       item.key === "mergeSearchProviders" ? "search providers" : "queries";
-    new BSN.Tooltip(document.getElementById(item.key), {
+    new BSN.Tooltip(popoverIcon, {
       customClass: "ml-1",
       title: `<div class='text-left'><div><strong>Merge:</strong> Adds ${providers} that aren't already in current list of ${providers}.</div><div><strong>Override:</strong> Replaces local ${providers} with new settings.</div><div><strong>Ignore:</strong> Keeps current list and ignores any incoming changes.</div></div>`,
     });
@@ -42,14 +50,14 @@ onMount(() => {
 {#if isCheckbox}
   <li class="list-group-item">
     <div class="form-check">
-      <input
-        type="checkbox"
-        class="form-check-input"
-        name="{item.key}"
-        id="settings_{item.key}"
-        checked="{value ? 'checked' : ''}"
-        on:change="{(e) => onChange(e.target.checked)}" />
-      <label class="form-check-label" for="settings_{item.key}">
+      <label class="form-check-label">
+        <input
+          type="checkbox"
+          class="form-check-input"
+          name="{item.key}"
+          checked="{value ? 'checked' : ''}"
+          on:change="{(e) => onChange(e.target.checked)}" />
+
         {item.label}
       </label>
     </div>
@@ -59,16 +67,15 @@ onMount(() => {
 {:else if isInput}
   <li class="list-group-item">
     <div class="form-group mb-0">
-      <label class="mb-1" for="settings_{item.key}">
+      <label class="mb-1">
         {item.label}
+        <input
+          type="text"
+          class="form-control"
+          name="{item.key}"
+          value="{value}"
+          on:input="{(e) => onChange(e.target.value)}" />
       </label>
-      <input
-        type="text"
-        class="form-control"
-        name="{item.key}"
-        id="settings_{item.key}"
-        value="{value}"
-        on:input="{(e) => onChange(e.target.value)}" />
     </div>
   </li>
 
@@ -77,19 +84,16 @@ onMount(() => {
   <li class="list-group-item">
     <span class="dropdown">
       <button
+        bind:this="{dropdownButton}"
         class="btn btn-outline-dark dropdown-toggle"
         type="button"
         name="{item.key}"
-        id="settings_{item.key}"
         data-bs-toggle="dropdown"
         aria-haspopup="true"
         aria-expanded="false">
         {item.menuItems.find((item) => item.key === value)?.label}
       </button>
-      <div
-        class="dropdown-menu"
-        tabindex="-1"
-        aria-labelledby="settings_{item.key}">
+      <div class="dropdown-menu" tabindex="-1">
         {#each item.menuItems as menuItem}
           <button
             on:click="{() => onChange(menuItem.key)}"
@@ -105,7 +109,11 @@ onMount(() => {
     <span class="form-check-label">
       {item.label}
     </span>
-    <a data-placement="right" id="{item.key}" data-toggle="tooltip"
-      ><i class="fas fa-info-circle text-info" aria-hidden="true"></i></a>
+    <i
+      bind:this="{popoverIcon}"
+      class="fas fa-info-circle text-info"
+      aria-hidden="true"
+      data-toggle="tooltip"
+      data-placement="right"></i>
   </li>
 {/if}
